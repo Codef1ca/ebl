cdde83f2ccf135f2cac5b4a2e5df69bb14aa8ddc
paular19 <ramospaula@gmail.com>
2025-06-12 13:41:22 -0300

    cambiostextos

--- app/page.tsx
+++ app/page.tsx
@@ -42,32 +42,32 @@ export default function Home() {
     <div className="container-txt">
     <h2 className="titulo-txt">{'//NOSOTROS'}</h2>
       <div className="container-row">
-      <p className="p-txt">Ibalance surge con la necesidad e idea de actualizar los abordajes metodológicos ya existentes, integrando conceptos claves para el entendimiento del ser humano en movimiento: </p>
+      <p className="p-txt">Ibalance surge con la idea de Surge con la idea de rediseñar y redefinir conceptos acerca del equilibrio y el aprendizaje del movimiento. Integrando conceptos claves para el entendimiento del ser humano en movimiento: </p>
       <p className="p-txt"><span className="negrita">Biotensegridad, Propiocepción y Núcleo core</span>, integramos estos conceptos para desarrollar todas las capacidades motoras.</p>
       </div>
-      <p className="p-txt2">Estos tres conceptos están íntimamente relacionados, haciendo que cada movimiento pueda ejecutarse de manera coordinada.</p>
+      <p className="p-txt2">Nuestro programa integra y aplica estos tres conceptos en cada ejercicio, bloque y sesión.</p>
     </div>
 
     <div className="flex w-full flex-col container-componen">
       <Tabs aria-label="Options" className="recuadro-com">
-        <Tab key="photos" title="BIOTENSEGRIDAD" className="titulo-componen">
+        <Tab key="photos" title="BIOTENSEGRIDAD:" className="titulo-componen">
           <Card className="card-componen">
             <CardBody className="card-componen">
-            Red o malla que envuelve todo el cuerpo, incluyendo órganos, músculos y articulaciones entre otras, gracias a ella, las estructuras músculo esqueléticas mantienen su forma ante una compresión o tracción repentina. 
+            Estructura viva: Este concepto viene de la palabra tensegridad, que surge de la arquitectura, en este caso aplicable al cuerpo humano, es una red o malla que envuelve todo el organismo, incluyendo músculos, tendones, articulaciones y órganos. Gracias a esta, las estructuras músculo esqueléticas mantienen su forma ante un desbalance o deformación repentina.
             </CardBody>
           </Card>
         </Tab>
         <Tab key="music" title="PROPIOCEPCIÓN">
           <Card className="card-componen">
             <CardBody>
-            Red que envuelve todo el cuerpo, (sistema miofascial) gracias a esta, las estructuras musculo esquelética mantienen su forma ante una compresión o tracción repentina, el cuerpo se auto estabiliza efectivamente.
+            Autopercepción: Es uno de los sentidos más importantes y activos en el cuerpo humano, a través del sistema nervioso, esta capacidad responde a receptores ligados a órganos y tejidos, esta capacidad brinda información precisa de la posición de cada parte del cuerpo en el espacio.
             </CardBody>
           </Card>
         </Tab>
-        <Tab key="videos" title="NÚCLEO CORE">
+        <Tab key="videos" title="CORE">
           <Card className="card-componen">
             <CardBody>
-            Como él mismo nombre lo indica, el el centro de nuestro cuerpo, entendemos a este núcleo más allá de las cadenas musculares que componen el centro, da origen al movimiento y a la estabilización, está íntimamente relacionado con todas las cadenas musculares, es lo que conecta la cintura escapulario (zona alta o tren superior) con la cintura pélvica (zona baja o tren inferior).
+            Núvleo o centro: Es el centro del cuerpo humano, más allá de las cadenas musculares que componen el centro, este núcleo da origen y estabilidad  a todo movimiento, acompaña la respiración, ya que el núcleo se activa en conjunto a todos los diafragmas. Conecta también la musculatura de la cintura escapular y pélvica.
             </CardBody>
           </Card>
         </Tab>
@@ -80,18 +80,25 @@ export default function Home() {
     <div className="ultimo-container">
     EXISTEN TRES NIVELES PARA ALCANZAR NUESTROS OBJETIVOS
     <div className="container-cuadritos">
-    <div className="cuadrito">APRENDER</div>
-    <div className="cuadrito2">PRACTICAR</div>
-    <div className="cuadrito3">ENTRENAR</div>
+    <div className="cuadrito">1° NIVEL: APRENDER</div>
+    <div className="cuadrito2">2° NIVEL: PRACTICAR</div>
+    <div className="cuadrito3">3° NIVEL: ENTRENAR</div>
     </div>
-    <h2 className="txt-ultimo">Nuestro objetivo siempre es el mismo: lograr efectos biológicos a largo plazo (E.B.L.P) y así también alcanzar armonía desde lo más profundo del cuerpo humano.</h2>
+    <h2 className="txt-ultimo">Primer nivel: desbloquear y desglosar los ejercicios para aprenderlos de manera sencilla, adaptamos cada ejercicio a cada persona según el nivel de práctica que ya traes.
+    <br></br>
+    Segundo nivel de nuestro programa, tiene duración variable, una vez que ejecutas y recuerdas  movimientos, secuencias y  bloques de cada sesión, ¡estás listo para avanzar al siguiente nivel!
+    <br></br>
+    Tercer nivel, durante esta Etapa logramos los efectos biológicos a largo plazo, ¡estamos listos para comenzar a complejizar la práctica y explorar tu máximo potencial!
+    </h2>
     </div>
 
 
     <div className="container-banda">
     <div className="stiker">PROGRAMA</div>
     <div className="banda">
-    Cada  programa de ejercicios está dividido en 4 bloques, a su vez el programa incluye 24 sesiones. Dentro de cada bloque aplicamos progresiones y variantes, adaptables según la experiencia y la necesidad de cada persona para una sencilla autogestión del programa en el que que vos elegís la modalidad para alcanzar los efectos.
+    Matemática de diseño del programa: 
+    Cada sesión tiene una duración de 60 minutos (cuatro bloques de 15 minutos), el equivalente a 270 acciones efectivas.
+    Programa completo: 24 horas, el equivalente a 8100 acciones efectivas.
     </div>
   <Image
     src="/conexion.png"
@@ -120,8 +127,8 @@ export default function Home() {
           className="personita"
         />
         <div className="flex flex-col">
-        <p className="text-md">SEGUIMIENTO SINCRÓNICO</p>
-        <p className="text-small text-default-500 text-card">24 sesiones · 2 veces por semana · 3 meses · 4 bloques de 15 minutos · 6 ejercicios · 27 ejecuciones · 270 acciones efectivas</p>
+        <p className="text-md">SEGUIMIENTO SINCRÓNICO 100%</p>
+        <p className="text-small text-default-500 text-card">2 SESIONES POR SEMANA DURANTE 3 MESES</p>
         </div>
       </CardHeader>
       <Divider />
@@ -146,18 +153,57 @@ export default function Home() {
           className="personita"
         />
         <div className="flex flex-col">
-          <p className="text-md">SEGUIMIENTO ASINCRÓNICO</p>
-          <p className="text-small text-default-500 text-card">6 clases de consulta durante 3 meses (1 cada 15 días) · 180 minutos en total con modalidad sincrónica</p>
+          <p className="text-md">SEGUIMIENTO  50%</p>
+          <p className="text-small text-default-500 text-card">Programa completo: 24 horas (6 horas online en vivo y 18 horas autoregistradas).
+            <br></br>
+            Incluye diseño de programa personalizado, evaluación y presentación final.
+            <br></br>
+            Duración variable: 2 o 3 meses (6 sesiones online en vivo, 12 sesiones autoregistrables)
+          </p>
         </div>
       </CardHeader>
       <Divider />
       <CardBody>
-      <p>U$D NO ME ACUERDO</p>
+      <p>U$D 180</p>
       </CardBody>
       <Divider />
       <CardFooter>
         <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui" className="link-color text-card">
           Visita nuestra plataforma de aprendizaje online
+        </Link>
+      </CardFooter>
+    </Card>
+     <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="heroui logo"
+          height={20}
+          src="/personita.png"
+          width={20}
+          className="personita"
+        />
+        <div className="flex flex-col">
+        <p className="text-md">CREA TU PROPIO SISTEMA</p>
+        <p className="text-small text-default-500 text-card">
+        Duración: 9 meses (108 horas). Incluye: sesiones teóricas, supervisión y acompañamiento en tu diseño.
+        <br></br>
+        Vos elegís un tema o una disciplina para transformar. Seguimiento 50-50.
+        <br></br>
+        3 clases por semana (2 prácticos y 1 conversatorio)
+
+        </p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <p>U$D 1620 (9 meses)</p>
+        <br></br>
+        <p>U$D 180 por mes</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui" className="link-color text-card">
+        Visita nuestra plataforma de aprendizaje online
         </Link>
       </CardFooter>
     </Card>
@@ -172,13 +218,14 @@ export default function Home() {
           className="personita"
         />
         <div className="flex flex-col">
-        <p className="text-md">WAKE UP</p>
-        <p className="text-small text-default-500 text-card">HACEME UNA PEQUEÑA DESCRIPCIÓN</p>
+        <p className="text-md">WAKE UP ACTIVATION - ACTIVACIONES DEL DESPERTAR</p>
+        <p className="text-small text-default-500 text-card">Sesiones especiales matutinas 100% sincrónico (3 clases por semana durante un mes / 12 sesiones de 15 minutos por sesión)
+</p>
         </div>
       </CardHeader>
       <Divider />
       <CardBody>
-        <p>U$D NO TENGO PRECIO</p>
+        <p>U$D 60</p>
       </CardBody>
       <Divider />
       <CardFooter>
@@ -199,12 +246,15 @@ export default function Home() {
         />
         <div className="flex flex-col">
         <p className="text-md">DISEÑO DEL PROGRAMA</p>
-        <p className="text-small text-default-500 text-card">Si ya nos conocemos podes acceder al diseño de un programa sin seguimiento</p>
+        <p className="text-small text-default-500 text-card">Incluye: encuesta diagnostica, diseño personalizado especial segúbn disciplina, versión analógica y digital
+          <br></br>
+          Duración: se actualiza cada mes, una vez cumplido el objetivo propuesto.
+        </p>
         </div>
       </CardHeader>
       <Divider />
       <CardBody>
-        <p>U$D 36</p>
+        <p>U$D 35</p>
       </CardBody>
       <Divider />
       <CardFooter>
@@ -224,13 +274,16 @@ export default function Home() {
           className="personita"
         />
         <div className="flex flex-col">
-        <p className="text-md">CLASE SUELTA</p>
-        <p className="text-small text-default-500 text-card">Si ya nos conocemos podes acceder a una clase suelta para evacuar dudas</p>
+        <p className="text-md">SESIÓN INDIVIDUAL</p>
+        <p className="text-small text-default-500 text-card">Incluye conversación introductoria, diseño de ejercicios y circuitos. 
+          <br></br>
+          Duración: 90 minutos. Horario a convenir.
+        </p>
         </div>
       </CardHeader>
       <Divider />
       <CardBody>
-        <p>U$D 8</p>
+        <p>U$D 18</p>
       </CardBody>
       <Divider />
       <CardFooter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./node_modules/@heroui/theme/dist/components/(button|card|code|divider|input|kbd|link|listbox|navbar|snippet|toggle|tabs|ripple|spinner|form|popover).js"
-],
+  ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [heroui()],
 };
+
+export default config;
